d441e88295637a2b2a58c84d5cd04aa277c9dd93
congcocz <congcong.c.zhang@nokia-sbell.com>
2020-12-21 19:49:30 +0800

    v1

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,29 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    return str.split('').reduce((rev,char) => char+rev,'');
+}
+
+module.exports = reverse;
+
+
+
+//Solution 1
+// function reverse(str) {
+//     return Array.from(str).reverse().join("");
+// }
+
+//Solution 2
+// function reverse(str) {
+//     let reversed = '';
+//     for(let c of str){
+//         reversed = c + reversed;
+//     }
+//     return reversed;
+// }

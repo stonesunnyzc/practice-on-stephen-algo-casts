3cfdd224779f59dbd5d0c5ceed4604aeb4003d76
congcocz <congcong.c.zhang@nokia-sbell.com>
2020-12-25 17:37:56 +0800

    chunk array and anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,32 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+
+
+// my answer
+  function anagrams(stringA, stringB) {
+    let regExp = /[\s]/g;
+    let strAHash = getCharsCountOfStr(stringA.replace(regExp, "").toLowerCase());
+    let strBHash = getCharsCountOfStr(stringB.replace(regExp, "").toLowerCase());
+    if (strAHash.size !== strBHash.size) return false;
+    for (let key in strAHash) {
+      if (strAHash[key] !== strBHash[key]) return false;
+    }
+    return true;
+  }
+  
+  function getCharsCountOfStr(str) {
+    const hash = new Map();
+    for (const char of str) {
+      if (hash[char]) {
+        hash.set(char, hash.get(char) + 1);
+      } else {
+        hash[char] = 1;
+        hash.set(char, 1);
+      }
+    }
+    return hash;
+  }
+    
 
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,47 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+
+// teacher's solution 2
+function chunk(array, size) {
+    let chunked = [];
+    let i = 0;
+    while(i<array.length){
+        chunked.push(array.slice(i,i+size));
+        i+=size;
+    }
+    return chunked;
+}
+
+
+// teacher's solution 1
+// function chunk(array, size) {
+//     let chunked = [];
+//     for(let ele of array){
+//         const last = chunked[chunked.length -1];
+//         if(!last || last.length === size){
+//             chunked.push([ele]);
+//         }else{
+//             last.push(ele);
+//         }
+//     }
+//     return chunked;
+// }
+
+
+// my answer
+// function chunk(array, size) {
+//     let res = [];
+//     if (array.length <= size) {
+//       res.push(array);
+//     } else {
+//       let i = 0;
+//       while (i * size < array.length) {
+//         res.push(array.slice(i * size, (i + 1) * size));
+//         i++;
+//       }
+//     }
+//     return res;
+// }
 
 module.exports = chunk;
